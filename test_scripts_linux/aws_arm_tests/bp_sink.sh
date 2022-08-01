22e7e1efba26e15a0c2775fa8e6f2d98cf82b04f
Prash Choksi <108488697+prashchoksiNASA@users.noreply.github.com>
2022-08-01 15:30:15 -0500

    Update bp_sink.sh
    LTP changes for bp_sink.sh

--- test_scripts_linux/aws_arm_tests/bp_sink.sh
+++ test_scripts_linux/aws_arm_tests/bp_sink.sh
@@ -2,12 +2,13 @@
 
 # path variables
 config_files=$HDTN_SOURCE_ROOT/tests/config_files
-sink_config=$config_files/inducts/bpsink_one_tcpclv4_port4558.json
+sink_config=$config_files/inducts/bpsink_one_stcp_port4558.json
 
 cd $HDTN_SOURCE_ROOT
 
 
 # BP Sink File
-./build/common/bpcodec/apps/bpsink-async --my-uri-eid=ipn:2.1 --inducts-config-file=$sink_config 
+./build/common/bpcodec/apps/bpsink-async --my-uri-eid=ipn:2.1 --inducts-config-file=$sink_config
+
 
 
